8c0fc238fb18843f9cf29a546f9222de9bbaa8d1
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-22 21:53:10 +0200

    Folow the steps

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,17 @@
+import _ from 'lodash';
+import './style.css';
+
+
+console.log("./src/index.js")
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
+ 
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+   element.classList.add('hello');
+
+  element.appendChild(myIcon);
     return element;
   }
-  
+ 
   document.body.appendChild(component());
